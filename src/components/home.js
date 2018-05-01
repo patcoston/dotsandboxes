79f77ec5431695060dd06ca2ba3c577c2920de09
Patrick Coston <patcoston@gmail.com>
2018-05-01 18:37:52 -0400

    Added navigation links

--- src/components/home.js
+++ src/components/home.js
@@ -1,9 +1,12 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 
 class Home extends Component {
     render() {
         return (
             <div>
+                <Link to="/Setup">Setup</Link><br />
+                <Link to="/Play">Play</Link>
             </div>
         )
     }
--- src/components/play.js
+++ src/components/play.js
@@ -1,9 +1,12 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 
 class Play extends Component {
     render() {
         return (
             <div>
+                <Link to="/">Home</Link><br />
+                <Link to="/Setup">Setup</Link>
             </div>
         )
     }
