32e7777d71dfb129b7565e4d740336ae34bae17b
Patrick Coston <patcoston@gmail.com>
2018-05-01 19:38:02 -0400

    Changed link text for Setup to Setup

--- src/components/home.js
+++ src/components/home.js
@@ -5,7 +5,7 @@ class Home extends Component {
     render() {
         return (
             <div>
-                <Link to="/Setup">Go to Setup</Link><br />
+                <Link to="/Setup">Setup</Link><br />
                 <Link to="/Play">Play</Link>
             </div>
         )
