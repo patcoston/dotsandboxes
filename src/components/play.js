81df2429f5ccc0d8fd4de6a80c6f8a78f702f672
Patrick Coston <patcoston@gmail.com>
2018-05-03 20:41:39 -0400

    Resolve conflict

--- src/components/play.js
+++ src/components/play.js
@@ -9,7 +9,7 @@ class Play extends Component {
             <div>
                 <Link to="/">Home</Link><br />
                 <Link to="/Setup">Setup</Link><br />
-                <Table rows={this.props.size.tableRows} cols={this.props.size.tableCols}/>
+                <Table rows="7" cols="7" />
             </div>
         )
     }
