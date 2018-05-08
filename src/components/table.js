2ff47a65e7d595b2fe49cd5dcec414494e0d7bcb
Patrick Coston <patcoston@gmail.com>
2018-05-07 21:39:41 -0400

    Added click handler to table odd/even even/odd TDs

--- src/components/table.js
+++ src/components/table.js
@@ -5,8 +5,18 @@ let colKey = 0;
 class TableCols extends Component {
     render() {
         let cols = [];
-        for (let i = 0; i<this.props.cols; i++) {
-            cols.push(<td key={++colKey}></td>);
+        let rowEven = (parseInt(this.props.row) % 2) === 0;
+        for (let c = 0; c<this.props.cols; c++) {
+            let colEven = (c % 2) === 0;
+            // if row even and col not even or vice-versa
+            let click = (rowEven && !colEven) || (!rowEven && colEven);
+            if (click) {
+                cols.push(<td key={++colKey} onClick={() => {
+                    alert('click');
+                }}></td>);
+            } else {
+                cols.push(<td key={++colKey}></td>);
+            }
         }
         return cols;
     }
@@ -15,8 +25,8 @@ class TableCols extends Component {
 class TableRows extends Component {
     render() {
         let rows = [];
-        for (let i = 0; i<this.props.rows; i++) {
-            rows.push(<tr><TableCols key={++colKey} cols={this.props.cols} /></tr>);
+        for (let r = 0; r<this.props.rows; r++) {
+            rows.push(<tr><TableCols key={++colKey} cols={this.props.cols} row={r} /></tr>);
         }
         return rows;
     }
